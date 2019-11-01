020e09b4064723adeb7f34dea1eb758803f9847e
chenchao19910217 <chenchao@chenchaodeMacBook-Pro.local>
2019-11-01 18:26:38 +0800

    Initial commit

---
+++ README.md.py
@@ -0,0 +1,21 @@
+## 简单的使用方法：
+
+
+# 创建虚拟环境
+# 使用pip安装第三方依赖
+# 修改settings.example.py文件为settings.py
+# 运行migrate命令，创建数据库和数据表
+# 运行python manage.py runserver启动服务器
+#
+#
+# 路由设置：
+
+
+from django.contrib import admin
+from django.urls import path, include
+from login import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', include('login.urls')),
+]
